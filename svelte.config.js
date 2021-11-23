6459f7db19431416dccbeafff8a32284227ad5f9
thudbuzz <thudbuzz@pm.me>
2021-11-23 15:40:34 +0000

    changed back to vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,10 @@
 /** @type {import('@sveltejs/kit').Config} */
-import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: adapter({
-			// default options are shown
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+		adapter: vercel()
 	}
 };
 
