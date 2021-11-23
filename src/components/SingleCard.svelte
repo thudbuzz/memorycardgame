26423744be7e2c2355bfd0ee48b14cb7f8bc2cab
thudbuzz <thudbuzz@pm.me>
2021-11-23 09:06:22 +0000

    initial commit

---
+++ src/components/SingleCard.svelte
@@ -0,0 +1,58 @@
+<script>
+	export let card;
+	export let handleChoice;
+	export let flipped;
+	export let disabled;
+
+	const handleClick = () => {
+		if (!disabled) {
+			handleChoice(card);
+		}
+	};
+</script>
+
+<div class="card">
+	<div class:flipped>
+		<svg class="front" viewbox="-10 -10 120 120">
+			<polygon points={card.points} fill={card.color} />
+		</svg>
+		<svg class="back" on:click={() => handleClick(card)} viewbox="0 0 100 100">
+			<rect x="0" y="0" width="100" height="100" fill="black" />
+		</svg>
+	</div>
+</div>
+
+<style>
+	.card {
+		position: relative;
+	}
+
+	.card img {
+		width: 100%;
+		height: 100%;
+		display: block;
+		border: 2px solid #fff;
+		border-radius: 6px;
+	}
+
+	.card .front {
+		position: absolute;
+		transform: rotateY(90deg);
+		transition: all ease-in 0.2s;
+	}
+
+	.flipped .front {
+		transform: rotateY(0deg);
+		transition-delay: 0.2s;
+	}
+
+	.card .back {
+		transition: all ease-in 0.2s;
+		transition-delay: 0.2s;
+	}
+
+	.flipped .back {
+		transform: rotateY(90deg);
+		transition-delay: 0s;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,117 @@
+<script>
+	import { onMount } from 'svelte';
+
+	import SingleCard from '/src/components/SingleCard.svelte';
+
+	const cardImages = [
+		{
+			description: 'square',
+			color: 'blue',
+			matched: false,
+			points: '0,0 100,0 100,100 0,100'
+		},
+		{
+			description: 'rectangle',
+			color: 'green',
+			matched: false,
+			points: '0,30 100,30 100,70 0,70'
+		},
+		{
+			description: 'triangle',
+			color: 'red',
+			matched: false,
+			points: '50,10 100,90 0,90'
+		},
+		{
+			description: 'shield',
+			color: 'orange',
+			matched: false,
+			points: '0,0 100,0 100,50 50,100 0,50'
+		},
+		{
+			description: 'diamond',
+			color: 'black',
+			matched: false,
+			points: '50,0 100,50 50,100 0,50'
+		},
+		{
+			description: 'cross',
+			color: 'purple',
+			matched: false,
+			points: '30,0 70,0 70,30 100,30 100,70 70,70 70,100 30,100 30,70 0,70 0,30 30,30'
+		}
+	];
+
+	$: cards = [];
+	$: turns = 0;
+	$: choiceOne = null;
+	$: choiceTwo = null;
+	$: disabled = false;
+
+	const shuffleCards = () => {
+		const shuffledCards = [...cardImages, ...cardImages]
+			.sort(() => Math.random() - 0.5)
+			.map((card) => ({ ...card, id: Math.random() }));
+
+		cards = shuffledCards;
+		turns = 0;
+	};
+
+	const handleChoice = (card) => {
+		choiceOne ? (choiceTwo = card) : (choiceOne = card);
+		if (choiceTwo && choiceTwo) {
+			disabled = true;
+			if (choiceOne.description === choiceTwo.description) {
+				cards = cards.map((card) => {
+					if (card.description === choiceOne.description) {
+						return { ...card, matched: true };
+					} else {
+						return { ...card };
+					}
+				});
+				resetTurn();
+			} else {
+				setTimeout(() => resetTurn(), 1000);
+			}
+		}
+	};
+
+	const resetTurn = () => {
+		choiceOne = null;
+		choiceTwo = null;
+		turns++;
+		disabled = false;
+	};
+
+	onMount(() => {
+		shuffleCards();
+	});
+</script>
+
+<div class="wrapper">
+	<button on:click={shuffleCards}>New Game</button>
+	<p>Turns:{turns}</p>
+
+	<div class="card-grid">
+		{#each cards as card (card.id)}
+			<SingleCard
+				{card}
+				{handleChoice}
+				flipped={card === choiceOne || card === choiceTwo || card.matched}
+				{disabled}
+			/>
+		{/each}
+	</div>
+</div>
+
+<style>
+	.wrapper {
+		max-width: 320px;
+	}
+	.card-grid {
+		margin-top: 40px;
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr 1fr;
+		grid-gap: 0.25em;
+	}
+</style>
